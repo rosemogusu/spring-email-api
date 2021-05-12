6585de962833112dd32b6952ae2961580c63488f
Rose Mogusu <rosemogusu@adalabsafrica.com>
2021-05-12 16:56:27 +0300

    Create all the packages.

---
+++ src/main/java/com/javabydeveloper/demo/SpringBootEmailTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.javabydeveloper.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabydeveloper/SpringBootEmailTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabydeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
